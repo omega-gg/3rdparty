b85ee4f8534f3ed28c0fe39128fabddd36fca616
bunjee <bunjee@omega.gg>
2020-02-07 10:18:41 +0100

    Update scripts

--- generate.sh
+++ generate.sh
@@ -138,39 +138,6 @@ elif [ $os = "android" ]; then
 fi
 
 #--------------------------------------------------------------------------------------------------
-# Artifact
-#--------------------------------------------------------------------------------------------------
-
-if [ "$2" != "build" -a "$2" != "clean" ]; then
-
-    echo "ARTIFACT 3rdparty-$1"
-    echo $thirdparty_url
-
-    thirdparty_url=$(getSource $thirdparty_url 3rdparty-$1)
-
-    echo ""
-    echo "DOWNLOADING 3rdparty-$1"
-    echo $thirdparty_url
-
-    curl -L -o 3rdparty.zip $thirdparty_url
-
-    echo ""
-    echo "EXTRACTING 3rdparty-$1"
-
-    unzip -q 3rdparty.zip
-
-    rm 3rdparty.zip
-
-    rm -rf $1
-
-    unzip -q 3rdparty-$1/3rdparty.zip
-
-    rm -rf 3rdparty-$1
-
-    exit 0
-fi
-
-#--------------------------------------------------------------------------------------------------
 # Clean
 #--------------------------------------------------------------------------------------------------
 
@@ -194,6 +161,37 @@ echo ""
 if [ $1 = "linux" ]; then
 
     sh install.sh $1
+
+    exit 0
+fi
+
+#--------------------------------------------------------------------------------------------------
+# Artifact
+#--------------------------------------------------------------------------------------------------
+
+if [ "$2" != "build" -a "$2" != "clean" ]; then
+
+    echo "ARTIFACT 3rdparty-$1"
+    echo $thirdparty_url
+
+    thirdparty_url=$(getSource $thirdparty_url 3rdparty-$1)
+
+    echo ""
+    echo "DOWNLOADING 3rdparty-$1"
+    echo $thirdparty_url
+
+    curl -L -o 3rdparty.zip $thirdparty_url
+
+    echo ""
+    echo "EXTRACTING 3rdparty-$1"
+
+    unzip -q 3rdparty.zip
+
+    rm 3rdparty.zip
+
+    unzip -qo 3rdparty-$1/3rdparty.zip
+
+    rm -rf 3rdparty-$1
 
     exit 0
 fi
