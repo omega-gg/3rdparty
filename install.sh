83e19bafcb4c315003b22c62232e01cfd99a381e
bunjee <bunjee@omega.gg>
2020-08-13 16:22:15 +0200

    Update to Ubuntu 20.04 (41)

--- install.sh
+++ install.sh
@@ -192,7 +192,7 @@ export DEBIAN_FRONTEND=noninteractive
 # NOTE: This is required to avoid getting prompted.
 #sudo printf "KBLAYOUT='us'\nXKBVARIANT='intl'\nBACKSPACE='guess'" > /etc/default/keyboard
 
-#cat /etc/default/keyboard
+sudo touch /etc/default/keyboard
 
 L='us' && sudo sed -i 's/XKBLAYOUT=\"\w*"/XKBLAYOUT=\"'$L'\"/g' /etc/default/keyboard
 
