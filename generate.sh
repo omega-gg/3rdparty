81135dd7fd83944027ea38ac7be352b59e364c77
bunjee <bunjee@omega.gg>
2020-12-21 12:45:24 +0100

    VLC/android: Update to 3.3.3

--- generate.sh
+++ generate.sh
@@ -43,7 +43,7 @@ SDK_version="29"
 NDK_versionA="21"
 NDK_versionB="21.1.6352462"
 
-VLC_version_android="3.2.12"
+VLC_version_android="3.3.3"
 
 #--------------------------------------------------------------------------------------------------
 # environment
