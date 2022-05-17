0928180fa78a13ec2f11fe6d745c2906751efc0c
bunjee <bunjee@omega.gg>
2022-05-17 16:09:46 +0200

    Qt: Add QtMultimedia (2)

--- generate.sh
+++ generate.sh
@@ -15,10 +15,10 @@ Qt5_version="5.15.2"
 Qt5_modules="qtbase qtdeclarative qtxmlpatterns qtsvg qtmultimedia"
 
 Qt6_version="6.2.1"
-Qt6_modules="qtbase qtdeclarative qtsvg qtmultimedia qt5compat qtshadertools"
+Qt6_modules="qtbase qtdeclarative qtsvg addons.qtmultimedia qt5compat qtshadertools"
 
 SSL_versionA="1.0.2u"
-SSL_versionB="1.1.1m"
+SSL_versionB="1.1.1o"
 SSL_versionC="1.1"
 
 VLC_version="3.0.16"
--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ set -e
 Qt4_version="4.8.7"
 Qt5_version="5.15.2"
 
-SSL_version="1.1.1m"
+SSL_version="1.1.1o"
 
 VLC_version="3.0.16"
 
