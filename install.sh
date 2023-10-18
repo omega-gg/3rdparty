67ee6c237693aef8e6f54996bbf0f72efa53403a
bunjee <bunjee@omega.gg>
2023-10-18 10:26:42 +0200

    qt: Add qtwebp

--- install.sh
+++ install.sh
@@ -430,6 +430,7 @@ if [ $platform = "linux32" -a $qt = "qt5" ]; then
 
     sudo cp "$lib"/qt5/plugins/imageformats/libqsvg.so  "$Qt5"/plugins/imageformats
     sudo cp "$lib"/qt5/plugins/imageformats/libqjpeg.so "$Qt5"/plugins/imageformats
+    sudo cp "$lib"/qt5/plugins/imageformats/libqwebp.so "$Qt5"/plugins/imageformats
 
     sudo cp "$lib"/qt5/plugins/mediaservice/libgstcamerabin.so "$Qt5"/plugins/mediaservice
 
