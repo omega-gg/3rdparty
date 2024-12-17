8171ba675ce66afad96a0849ef4f6e43b371e0a4
bunjee <bunjee@omega.gg>
2024-12-17 17:32:14 +0100

    artifact 8094

--- generate.sh
+++ generate.sh
@@ -6,7 +6,7 @@ set -e
 #--------------------------------------------------------------------------------------------------
 
 # NOTE: Also check Sky_artifact.
-artifact="8081"
+artifact="8094"
 
 source="http://omega.gg/get/Sky/3rdparty"
 
