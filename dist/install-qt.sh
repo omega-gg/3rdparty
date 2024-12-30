3917811b9e4533388f1ab5b120b36ff6ef9d478e
bunjee <bunjee@omega.gg>
2024-12-30 14:26:37 +0100

    Qt6 6.8.1 (22)

--- dist/install-qt.sh
+++ dist/install-qt.sh
@@ -410,15 +410,12 @@ for COMPONENT in ${COMPONENTS}; do
     # NOTE Qt6.7.0: The folder is flattened in the archive.
     if [[ "${VERSION}" > "6.6.0" ]]; then
         path="${UNPACK_DIR}/${VERSION}/${SUBDIR}"
-        ls -la "${UNPACK_DIR}"
-        ls -la "${UNPACK_DIR}/bin"
         mv "${UNPACK_DIR}" temp
-        ls -la temp
         mkdir -p "$path"
         mv temp/* "$path"
         rm -rf temp
-        echo "$path"
-        ls -la "$path"
+        ls -la "$path"/bin
+        echo "$PWD"
     fi
 
     #
