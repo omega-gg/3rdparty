baaf11a6de7bb1aacd3e919eb5f5b13664c2c74c
bunjee <bunjee@omega.gg>
2025-03-12 14:40:55 +0100

    artifact 8487 (2)

--- install.sh
+++ install.sh
@@ -146,8 +146,10 @@ libvlccore_version="$libvlccore_versionA"
 #--------------------------------------------------------------------------------------------------
 
 # NOTE: libpulse-dev and libgstreamer(s) are needed for libQt5Multimedia.
+# NOTE Qt6: libxkbcommon* is required for libQt6Gui.
 X11_linux="libx11-dev libxi-dev libxinerama-dev libxrandr-dev libxcursor-dev libfontconfig-dev "\
-"libaudio2 libpulse-dev libgstreamer1.0-0 libgstreamer-plugins-base1.0-0"
+"libaudio2 libpulse-dev libgstreamer1.0-0 libgstreamer-plugins-base1.0-0 "\
+"libxkbcommon-dev libxkbcommon-x11-dev"
 
 if [ $qt != "qt4" ]; then
 
