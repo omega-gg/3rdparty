60c54ff4802a68fed860ece0834e637bd85acd7e
bunjee <bunjee@omega.gg>
2025-03-26 14:53:47 +0100

    generate: Update VLC

--- generate.sh
+++ generate.sh
@@ -315,7 +315,7 @@ extractVlc()
 
         # FIXME android/VLC 3: We need a recent libc++_shared when building with NDK26+, so we
         #                      copy the one that comes with VLC 4.
-        if [ $qt = "qt6" ]; then
+        if [ $3 = "$VLC4_version" -a $qt = "qt6" ]; then
 
             copyVlcShared armeabi-v7a "$VLC3"
             copyVlcShared arm64-v8a   "$VLC3"
