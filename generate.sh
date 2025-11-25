be65e239643a423f2065a5b23200b7f353fc1ea0
bunjee <bunjee@omega.gg>
2025-11-25 11:41:06 +0100

    macOS: Fix packaging

--- generate.sh
+++ generate.sh
@@ -537,12 +537,7 @@ if [ $host = "macOS" ]; then
     grep="ggrep"
 
     # NOTE macOS: python3 packaging module is required for devices.py.
-
-    python3 -m venv .venv
-
-    . ".venv/bin/activate"
-
-    pip install packaging
+    python3 -m pip install packaging
 else
     grep="grep"
 fi
