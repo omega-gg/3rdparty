58e91f32480f259226113000978d820a8eab1a38
bunjee <bunjee@omega.gg>
2025-12-18 16:56:47 +0100

    artifact 9433

--- generate.sh
+++ generate.sh
@@ -6,7 +6,7 @@ set -e
 #--------------------------------------------------------------------------------------------------
 
 # NOTE: Also check Sky_artifact.
-artifact="9418"
+artifact="9433"
 
 source="https://omega.gg/get/Sky/3rdparty"
 
