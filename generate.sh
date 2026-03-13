9bb2c6ab7ef664fb6dbbec9fb6b5280d5faae992
Benjamin Arnaud <bunjee@omega.gg>
2026-03-13 11:34:02 +0100

    artifact 9905

--- generate.sh
+++ generate.sh
@@ -6,7 +6,7 @@ set -e
 #--------------------------------------------------------------------------------------------------
 
 # NOTE: Also check Sky_artifact.
-artifact="9892"
+artifact="9905"
 
 source="https://omega.gg/get/Sky/3rdparty"
 
